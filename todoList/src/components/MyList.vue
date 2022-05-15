85c474c834bee5c1bd661944255d0d7955aaa58d
limo821 <1519083516@qq.com>
2022-05-15 22:50:18 +0800

    todoList-firstcommit

---
+++ todoList/src/components/MyList.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul :class="changeClass">
+    <MyItem 
+      v-for="todo in todos" 
+      :key="todo.id" 
+      :todo="todo" 
+      :deleteTodo="deleteTodo"
+      :checkTodo="checkTodo">
+    </MyItem>
+  </ul>
+</template>
+
+<script>
+import MyItem from "./MyItem";
+
+export default {
+  name: "MyList",
+  components: { MyItem },
+  props:['todos','checkTodo','deleteTodo'],
+  computed:{
+    changeClass(){
+      return this.todos.length==0?'todo-empty':'toto-main';
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* main */
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
